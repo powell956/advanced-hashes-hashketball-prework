dc09a6d62cc078d44dc189d0f4c3caa5d83f0446
powell956 <powell956@gmail.com>
2018-12-05 16:37:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,13 @@
-# Write your code here!
-
-
-
-
-
-
-
-
-
-
+def game_hash
+  game = {
+    :home => {
+      team_name: "Brooklyn Nets",
+      colors: "Black, White",
+      players:
+    },
+    :away => {
+      team_name: ,
+      colors: ,
+      players:
+    }
+  }
